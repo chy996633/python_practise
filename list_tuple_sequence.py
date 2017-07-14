064050059a925bf93273b5bd52228df9550020b7
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-07-14 05:08:48 -0500

    just practise

--- list_tuple_sequence.py
+++ list_tuple_sequence.py
@@ -1,9 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print(numbers)
+# print(numbers)
 
 format = "Pi with three decimals: %.3f"
 from math import pi
-print(format % pi)
+# print(format % pi)
+
+# print('%s plus %s equals %s' % (1, 2, 3))
+
+# print('Pi: %f...' % pi)
+
+print('%10f' % pi)
 
 
