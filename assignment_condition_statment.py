7c28b9cae91ac16db5c70471883c2e2d3a9aec41
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-07-21 02:51:51 -0500

    python practise

---
+++ assignment_condition_statment.py
@@ -0,0 +1,11 @@
+x, y ,z = 1, 2, 3
+print(x, y, z)
+
+values = 1, 2, 3
+print(values)
+x, y, z = values
+print(x, y, z)
+
+a, b, *rest = [1, 2, 3, 4, 5]
+print(a, b, rest)
+
---
+++ dict_methods.py
@@ -0,0 +1,17 @@
+a = {}.fromkeys(['name', 'age'])
+
+print(a)
+
+print(a.get('address'))
+
+
+robin = {'name' : 'Robin', 'girlfriend' : 'Marion'}
+key, value = robin.popitem()
+print("key: %s, value: %s" % (key, value))
+
+
+# error
+# x, y, z = 1, 2, 3, 4
+
+
+
