072c3a3211ec45ec7e20ffeec453849082fa7ef4
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-08-17 04:13:26 -0500

    learn python practise

---
+++ class_5.py
@@ -0,0 +1,31 @@
+# -- coding: utf-8 --
+
+my_name = '陈海越'
+
+my_age = 25
+
+my_height = 170
+
+my_weight = 58
+
+my_eyes = 'black'
+
+my_teeth = 'white'
+
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+
+print "He's %d inches tall." % my_height
+
+print "He's %d kg heavy." % my_weight
+
+print "Actually that's not too heavy."
+
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d and %d I get %d." % (my_age, my_weight, my_height, my_age + my_weight + my_height)
+
+
