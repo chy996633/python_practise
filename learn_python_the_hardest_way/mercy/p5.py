ca7d5109f3f2821936dd641c3ab82b1c135a460c
mercy <1243984690@qq.com>
2017-08-19 17:01:23 +0800

    add practice NO.5 file

---
+++ learn_python_the_hardest_way/mercy/p5.py
@@ -0,0 +1,16 @@
+#practice 5
+my_name='Mercy Li'
+my_age=25
+my_height=158 #cm
+my_weight=46 #kg
+my_eyes='Brown'
+my_teeth='White'
+my_hair='Black'
+
+print "Let's talk about %s" %my_name
+print"She's %d cm tall" %my_heigt
+print"She's %kg heavy," %my_weight
+print"Actually that's not too heavy."
+print"She's got %s eyes and %s hair." %(my_eyes,my_hair)
+print"Her teeth are usually %s depending on the tea." %my_teeth
+print"If i add %d, %d, and %d i get %d."%(my_age,my_height,my_weight,my_age+my_height+my_weight)
