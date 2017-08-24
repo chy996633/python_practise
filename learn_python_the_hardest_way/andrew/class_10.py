aa1213c5cbcb586ebde612113e682b0b11e90cad
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-08-24 02:24:05 -0500

    python practise

---
+++ learn_python_the_hardest_way/andrew/class_10.py
@@ -0,0 +1,45 @@
+print "I am %s tall." % "6'2\""
+
+print 'I am %s tall.' % '6\'2"'
+
+tabby_cat = "\tI'm tabbed in."
+
+persian_cat = "I'm split\non a line."
+
+backslash_cat = "I'm \\ a \\ act."
+
+fat_cat = """
+
+I'll do a list:
+
+\t* Cat food
+
+\t* Fishies
+
+\t* Catnip\n\t* Grass
+
+"""
+
+print tabby_cat
+
+print persian_cat
+
+print backslash_cat
+
+print fat_cat
+
+print "I am %r tall." % "6'2\""
+
+print 'I am %r tall.' % '6\'2"'
+
+slim_cat = '''
+
+I'll do a list:
+
+\t* Milk
+
+\t* Mouse
+
+'''
+
+print slim_cat
