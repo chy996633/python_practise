f9ac9fd1c5447f016bcdabf95d598b6680c256da
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-09-07 05:17:47 -0500

    add movement

--- PyGame/demo.py
+++ PyGame/demo.py
@@ -4,7 +4,8 @@ import pygame
 from pygame.locals import *
 
 # 2 Initialize the game
-
+keys = [False, False, False, False]
+playerpos = [100, 100]
 pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
@@ -26,7 +27,7 @@ while 1:
     screen.blit(castle, (0, 135))
     screen.blit(castle, (0, 240))
     screen.blit(castle, (0, 345))
-    screen.blit(player, (100, 100))
+    screen.blit(player, playerpos)
 
     pygame.display.flip()
 
@@ -34,3 +35,33 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_w:
+                keys[0] = True
+            if event.key == K_a:
+                keys[1] = True
+            if event.key == K_s:
+                keys[2] = True
+            if event.key == K_d:
+                keys[3] = True
+        if event.type == pygame.KEYUP:
+            if event.key == K_w:
+                keys[0] = False
+            if event.key == K_a:
+                keys[1] = False
+            if event.key == K_s:
+                keys[2] = False
+            if event.key == K_d:
+                keys[3] = False
+
+    if keys[0] == True:
+        if playerpos[1] > 0: playerpos[1] -= 5
+    if keys[1] == True:
+        if playerpos[0] > 0: playerpos[0] -= 5
+    if keys[2] == True:
+        if playerpos[1] < height - player.get_height(): playerpos[1] += 5
+    if keys[3] == True:
+        if playerpos[0] < width - player.get_width(): playerpos[0] += 5
+
+
+
