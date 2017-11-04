5dc660bed354db74f101eeea1bc380f1819ab391
Andrew chen <chy996633@qq.com>
2017-11-04 12:27:00 +0800

    add bow shooting and bad guy moving

--- learn_python_the_hardest_way/mercy/game/game.py
+++ learn_python_the_hardest_way/mercy/game/game.py
@@ -2,6 +2,7 @@
 import pygame
 import math
 from pygame.locals import *
+import random
 
 #2 - Initialize the game
 pygame.init() 
@@ -9,14 +10,24 @@ width,height = 640,480
 screen = pygame.display.set_mode((width,height))
 keys = [False,False,False,False]
 playerpos = [100,100]
-
+acc = [0,0]
+arrows = []
+badtimer = 100
+badtimer1 = 0
+badguys = [[640,100]]
+healthvalue =194
 #3 - load images
 player = pygame.image.load("resources/images/dude.png")
 grass = pygame.image.load("resources/images/grass.png")
 castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
+badguyimg1 = pygame.image.load("resources/images/badguy.png")
+badguyimg = badguyimg1
+
 
 #4 - keep looping through
 while 1:
+	badtimer -= 1
 	#5 - clear the screen before drawing it again
 	screen.fill(0)
 	#6 - draw the screen elements
@@ -32,10 +43,49 @@ while 1:
 	position = pygame.mouse.get_pos()
 	angle = math.atan2(position[1]-(playerpos[1]+32),position[0]-(playerpos[0]+26))
 	playerrot = pygame.transform.rotate(player,360-angle*57.29)
-	print angle*57.29
+	#print angle*57.29
 	playerpos1 = (playerpos[0]-playerrot.get_rect().width/2,playerpos[1]-playerrot.get_rect().height/2)
-	print playerrot.get_rect().width,playerrot.get_rect().height, " mouse position:", position, " playerrot position:", playerpos1
+	#print playerrot.get_rect().width,playerrot.get_rect().height, " mouse position:", position, " playerrot position:", playerpos1
 	screen.blit(playerrot,playerpos1)
+	#6.2-Draw arrows
+	for bullet in arrows:
+		index = 0
+		velx = math.cos(bullet[0])*10
+		vely = math.sin(bullet[0])*10
+		bullet[1] += velx
+		bullet[2] += vely
+		if bullet[1] < -64 or bullet[1] > 640 or bullet[2] < -64 or bullet[2]>480:
+			arrows.pop(index)
+		index += 1
+		# arrow1 = pygame.transform.rotate(arrow,360-bullet[0]*57.29)
+		# screen.blit(arrow1,(bullet[1],bullet[2]))
+		for projectile in arrows:
+			arrow1 = pygame.transform.rotate(arrow,360-projectile[0]*57.29)
+			screen.blit(arrow1,(projectile[1],projectile[2]))
+	#6.3-Draw badguys
+	if badtimer == 0:
+		badguys.append([640,random.randint(50,430)])
+		badtimer = 100-(badtimer1*2)
+		if badtimer1 >= 35:
+			badtimer1 = 35
+		else:
+			badtimer1 += 5
+	index = 0
+	for badguy in badguys:
+		if badguy[0] < -64:
+			badguys.pop(index)
+		badguy[0] -=7
+		#6.3.1 Attack castle
+		badrect = pygame.Rect(badguyimg.get_rect())
+		badrect.top = badguy[1]
+		badrect.left = badguy[0]
+		if badrect.left < 64:
+			healthvalue -=random.randint(5,20)
+			badguys.pop(index)
+		index += 1
+	for badguy in badguys:
+		screen.blit(badguyimg,badguy)
+
 	#7 - update the screen
 	pygame.display.flip()
 	#8 - loop through the events
@@ -45,7 +95,6 @@ while 1:
 			#if it is quit the game
 			pygame.quit()
 			exit(0)
-			print"2"
 		if event.type == pygame.KEYDOWN:
 			if event.key == K_w:
 				keys[0] = True
@@ -64,6 +113,10 @@ while 1:
 				keys[2] = False
 			elif event.key == pygame.K_d:
 				keys[3] = False
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			position = pygame.mouse.get_pos()
+			acc[1] += 1
+			arrows.append([math.atan2(position[1]-(playerpos1[1]+32),position[0]-(playerpos1[0]+26)),playerpos1[0]+32,playerpos1[1]+32])
 # 9 -Move player
 	if keys[0]:
 		playerpos[1] -= 5
