66871e706a17fec23a3609059667aba006069288
mercy <1243984690@qq.com>
2017-11-10 17:22:01 +0800

    add the code of the healthbar and time

--- learn_python_the_hardest_way/mercy/game/game.py
+++ learn_python_the_hardest_way/mercy/game/game.py
@@ -23,6 +23,8 @@ castle = pygame.image.load("resources/images/castle.png")
 arrow = pygame.image.load("resources/images/bullet.png")
 badguyimg1 = pygame.image.load("resources/images/badguy.png")
 badguyimg = badguyimg1
+healthbar = pygame.image.load("resources/images/healthbar.png")
+health = pygame.image.load("resources/images/health.png")
 
 
 #4 - keep looping through
@@ -82,9 +84,31 @@ while 1:
 		if badrect.left < 64:
 			healthvalue -=random.randint(5,20)
 			badguys.pop(index)
+		#6.3.2 check for collosions
+		index1 = 0
+		for bullet in arrows:
+			bullrect = pygame.Rect(arrow.get_rect())
+			bullrect.left = bullet[1]
+			bullrect.top = bullet[2]
+			if badrect.colliderect(badrect):
+				acc[0] += 1
+				badguys.pop(index)
+				arrows.pop(index1)
+		#6.3.3- next bad guy
 		index += 1
 	for badguy in badguys:
 		screen.blit(badguyimg,badguy)
+	#6.4 Draw clock
+	font = pygame.font.Font(None,24)
+	survivedtext = font.render(str((90000-pygame.time.get_ticks())/60000)+":"+str((90000-pygame.time.get_ticks())/1000%60).zfill(2),True,(0,0,0))
+	textRect = survivedtext.get_rect()
+	textRect.topright = [635,5]
+	screen.blit(survivedtext,textRect)
+	#6.5 Draw health bar
+	screen.blit(healthbar,(5,5))
+	for health1 in range(healthvalue):
+		screen.blit(health,(health1+8,8))
+
 
 	#7 - update the screen
 	pygame.display.flip()
